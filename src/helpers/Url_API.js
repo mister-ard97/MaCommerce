5eb1bb2d2d58c7a5c068d2f00e50f44b592d1f3d
mister-ard97 <rezardiansyah1997@gmail.com>
2019-10-09 11:47:37 +0700

    testing

--- src/helpers/Url_API.js
+++ src/helpers/Url_API.js
@@ -1,2 +1,2 @@
-// export const URL_API = 'http://localhost:2002';
-export const URL_API = 'https://testingdb-app.herokuapp.com'
+export const URL_API = 'http://localhost:2002';
+//export const URL_API = 'https://testingdb-app.herokuapp.com'
